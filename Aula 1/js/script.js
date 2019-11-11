13381aea95c0043b057e1649d46cd7753dd68dee
Denilson Santos <denilson.avsantos@hotmail.com>
2019-11-11 14:03:34 -0200

    Escolhendo o carro e a cor e manipuando a DOM

--- Aula 1/js/script.js
+++ Aula 1/js/script.js
@@ -53,7 +53,33 @@ function soma(numero1, numero2) {
 
 // for com DOM
 
-for (var i = 0; i <= 10; i++) {
-    document.write("Denilson");
+// for (var i = 0; i <= 10; i++) {
+//     document.write("Denilson");
+// }
+
+// var carro1 = "Volvo";
+// var corC1 = "red";
+
+// var carro2 = "Gol";
+// var corC2 = "Verde";
+
+// document.write(carro1, corC1);
+// documento.bgColor = corC1;
+// document.write(carro2, corC2);
+// documento.bgColor = corC2;
+
+console.log("Carros Disponiveis : Volvo na cor vermelha e gol na cor verde");
+
+let carro = prompt("Digite o Carro 1");
+let corC = prompt("Digite a cor do carro");
+
+if (carro == "volvo" && corC == "red") {
+    document.write("Volvo na cor vermelha");
+    documento.bgColor = "corC";
+} else {
+    document.write("Gol na cor verde");
+    documento.bgColor = "corC";
 }
+
+
 
