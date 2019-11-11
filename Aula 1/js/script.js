7ec01469913974850e5bb2f18e5dbe9e978ddc08
Denilson Santos <denilson.avsantos@hotmail.com>
2019-11-11 11:49:14 -0200

    Dados da aula 1

---
+++ Aula 1/js/script.js
@@ -0,0 +1,27 @@
+//Declarar variaveis
+
+var length = 21;//numeros
+var sobrenome = "Santos";//String
+var carros = ["Saab", "Volvo","BMW"];//Array
+var x = {primeironome:"Denilson", ultimonome:"Santos"};//Objeto
+
+//Função
+function soma(numero1, numero2){
+    return numero1 + numero2;
+}
+
+soma(1,2); //Usar uma função
+
+//invocando uma função antes de definir
+soma(1,3);
+function soma(numero1, numero2){
+    return numero1 + numero2;
+}
+
+// 
+
+
+
+
+
+
